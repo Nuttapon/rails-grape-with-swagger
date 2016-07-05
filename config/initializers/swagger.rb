9f9ad38606753e9a6fec69e5a402b2ea0b248e1c
nut <nut@omise.co>
2016-07-05 14:19:39 +0700

    Update grape_app_url

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.app_name = "User Management"
 GrapeSwaggerRails.options.url      = "/api/v1/docs"
-GrapeSwaggerRails.options.app_url  = "http://localhost:3000"
+GrapeSwaggerRails.options.app_url  = Rails.application.secrets.grape_app_url
